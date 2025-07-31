fe870336881a620640e389074da4fe21fe7fd32a
Arshad.N <arshadaslam989@gmail.com>
2025-07-31 10:27:19 +0530

    7/31/2025

---
+++ src/App.jsx
@@ -0,0 +1,57 @@
+import './App.css'
+import { Formik, Form, Field } from 'formik';
+import { SignupValidation } from './SignupValidation';
+
+const initialValues = {
+  name: '',
+  email: '',
+  password: '',
+  cpassword: ''
+}
+
+function App() {
+  return (
+    <div className='App'>
+      <Formik
+        initialValues={initialValues}
+        validationSchema={SignupValidation}
+      >
+        {({ errors }) => (
+          <Form >
+            <label htmlFor="name">Name</label>
+            <br />
+            <Field type="text" name="name"></Field>
+            <br />
+            {errors.name && <small>{errors.name}</small>}
+            <br />
+
+            <label htmlFor="email">Email</label>
+            <br />
+            <Field type="email" name="email"></Field>
+            <br />
+            {errors.email && <small>{errors.email}</small>}
+            <br />
+
+            <label htmlFor="password">Password</label>
+            <br />
+            <Field type="password" name="password"></Field>
+            <br />
+            {errors.password && <small>{errors.password}</small>}
+            <br />
+
+            <label htmlFor="cpassword">Confirm Password</label>
+            <br />
+            <Field type="password" name="cpassword"></Field>
+            <br />
+            {errors.cpassword && <small>{errors.cpassword}</small>}
+            <br />
+            
+            <button type='submit'>Submit</button>
+          </Form>
+        )}
+      </Formik>
+    </div>
+  )
+}
+
+export default App
